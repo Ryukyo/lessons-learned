9d0e695346c6f6ef544939966b47db73dc9a0aba
Ryukyo <florian.fejer@hotmail.de>
2023-02-19 15:46:24 +0900

    initial setup as by start spring

---
+++ src/main/java/com/ryukyo/lessonslearned/LessonslearnedApplication.java
@@ -0,0 +1,13 @@
+package com.ryukyo.lessonslearned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonslearnedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonslearnedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryukyo/lessonslearned/LessonslearnedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryukyo.lessonslearned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonslearnedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
