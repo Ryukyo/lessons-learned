061ff612e15df284a6fd4264318b559b11c204f5
Ryukyo <florian.fejer@hotmail.de>
2023-02-21 21:46:14 +0900

    Add spring security and permit existing paths

---
+++ src/main/java/com/ryukyo/lessonslearned/config/ProjectSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ryukyo.lessonslearned.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig  {
+
+    /**
+     * @param http
+     * @return SecurityFilterChain
+     * @throws Exception
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // Enable when not using th:action in form which already provides CSRF protection
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                .and().formLogin().and().httpBasic();
+        return http.build();
+    }
+
+}
