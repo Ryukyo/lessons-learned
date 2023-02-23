addf90ed34c920136a27f5bacc0cb9654bc4ef24
Ryukyo <florian.fejer@hotmail.de>
2023-02-23 16:20:02 +0900

    Add login/logout controller and form

--- src/main/java/com/ryukyo/lessonslearned/config/ProjectSecurityConfig.java
+++ src/main/java/com/ryukyo/lessonslearned/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.ryukyo.lessonslearned.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,15 +21,36 @@ public class ProjectSecurityConfig  {
         // Enable when not using th:action in form which already provides CSRF protection
         http.csrf().disable()
                 .authorizeHttpRequests()
+                .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/saveMsg").permitAll()
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
+                .requestMatchers("/login").permitAll()
                 .requestMatchers("/assets/**").permitAll()
-                .and().formLogin().and().httpBasic();
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().httpBasic();
         return http.build();
+    }
+
+//    temporary method to handle login until DB is available
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
