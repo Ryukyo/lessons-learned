4c17acef6a4d1edc64728892a17d67be855a076f
Ryukyo <florian.fejer@hotmail.de>
2023-02-26 23:14:09 +0900

    Optional changes after migration to Spring Data JPA

--- src/main/java/com/ryukyo/lessonslearned/LessonsLearnedApplication.java
+++ src/main/java/com/ryukyo/lessonslearned/LessonsLearnedApplication.java
@@ -2,8 +2,12 @@ package com.ryukyo.lessonslearned;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.ryukyo.lessonslearned.repository")
+@EntityScan("com.ryukyo.lessonslearned.model")
 public class LessonsLearnedApplication {
 
 	public static void main(String[] args) {
