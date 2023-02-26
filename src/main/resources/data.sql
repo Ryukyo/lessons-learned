1eb5014515bd7efeee9b9b9e8073de00baae525e
Ryukyo <florian.fejer@hotmail.de>
2023-02-26 11:35:24 +0900

    Prepare DB schemas and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Feb 16 ','Carnival''s Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Sep 20 ','World Children Day','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Oct 31 ','Halloween','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Dec 24 ','Christmas','FESTIVAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Jan 1 ','New Years Day','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Apr 10 ','Easter Monday','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+ VALUES (' Dec 25 ','1st Christmas Holiday','FEDERAL',CURDATE(),'DBA');
+
+INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
+  VALUES (' Dec 26 ','2nd Christmas Holiday','FEDERAL',CURDATE(),'DBA');
