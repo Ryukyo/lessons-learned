020de4946b16be2e6ccd2022fc073006fa163557
Ryukyo <florian.fejer@hotmail.de>
2023-07-06 19:54:38 +0900

    Update application props and logging settings

--- src/main/java/com/ryukyo/lessonslearned/controller/DashboardController.java
+++ src/main/java/com/ryukyo/lessonslearned/controller/DashboardController.java
@@ -5,6 +5,8 @@ import com.ryukyo.lessonslearned.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +15,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class DashboardController {
+
     @Autowired
     PersonRepository personRepository;
 
+    @Value("${lessonslearned.pageSize}")
+    private int defaultPageSize;
+
+    @Value("${lessonslearned.contact.successMsg}")
+    private String message;
+
+    @Autowired
+    Environment environment;
+
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model,Authentication authentication, HttpSession session) {
+    public String displayDashboard(Model model, Authentication authentication,
+        HttpSession session) {
         Person person = personRepository.readByEmail(authentication.getName());
         model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
-        if(null != person.getSchoolClass() && null != person.getSchoolClass().getName()){
+        if (null != person.getSchoolClass() && null != person.getSchoolClass().getName()) {
             model.addAttribute("enrolledClass", person.getSchoolClass().getName());
         }
         session.setAttribute("loggedInPerson", person);
         return "dashboard.html";
+    }
+
+    // testing
+    public void logMessages() {
+        log.error("defaultPageSize value with @Value annotation is : " + defaultPageSize);
+        log.error("successMsg value with @Value annotation is : " + message);
+
+        log.error("defaultPageSize value with Environment is : " + environment.getProperty(
+            "eazyschool.pageSize"));
+        log.error("successMsg value with Environment is : " + environment.getProperty(
+            "eazyschool.contact.successMsg"));
+        log.error(
+            "Java Home environment variable using Environment is : " + environment.getProperty(
+                "JAVA_HOME"));
     }
 
 }
--- src/main/java/com/ryukyo/lessonslearned/service/ContactService.java
+++ src/main/java/com/ryukyo/lessonslearned/service/ContactService.java
@@ -1,5 +1,6 @@
 package com.ryukyo.lessonslearned.service;
 
+import com.ryukyo.lessonslearned.config.LessonsLearnedProps;
 import com.ryukyo.lessonslearned.constants.SchoolConstants;
 import com.ryukyo.lessonslearned.model.Contact;
 import com.ryukyo.lessonslearned.repository.ContactRepository;
@@ -17,8 +18,13 @@ public class ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
+
+    @Autowired
+    LessonsLearnedProps lessonsLearnedProps;
+
     /**
      * Save Contact Details into DB
+     *
      * @param contact
      * @return boolean
      */
@@ -33,7 +39,11 @@ public class ContactService {
     }
 
     public Page<Contact> findMsgsWithOpenStatus(int pageNum, String sortField, String sortDir) {
-        int pageSize = 5;
+        int pageSize = lessonsLearnedProps.getPageSize();
+        if (null != lessonsLearnedProps.getContact() && null != lessonsLearnedProps.getContact()
+            .get("pageSize")) {
+            pageSize = Integer.parseInt(lessonsLearnedProps.getContact().get("pageSize").trim());
+        }
         Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
             sortDir.equals("asc") ? Sort.by(sortField).ascending()
                 : Sort.by(sortField).descending());
